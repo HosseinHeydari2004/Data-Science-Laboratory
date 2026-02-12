358abbce22fc1483ac013ae6ec936ce98c4dc805
Hossein <hosseinheydari992020@gmail.com>
2026-02-12 18:48:03 +0330

    coding unique_values func, detect_outliers

--- Core/EDA/Eda_untils.py
+++ Core/EDA/Eda_untils.py
@@ -16,3 +16,16 @@ def detect_missing_values(df: pd.DataFrame):
 def describe_data(df: pd.DataFrame, include: str = "all"):
     """This function returns a general description of the data"""
     return df.describe(include=include).T
+
+
+def unique_values_data(df: pd.DataFrame, col: str):
+    return df[col].unique()
+
+
+def detect_outliers(df: pd.DataFrame, col: str)->pd.DataFrame:
+    Q1 = df[col].quantile(0.25)
+    Q3 = df[col].quantile(0.75)
+    IQR = Q3 - Q1
+    lower = Q1 - 1.5 * IQR
+    upper = Q3 + 1.5 * IQR
+    return df[(col < lower) | (col > upper)]
