286d53411f70f7ce31ab01ff33f6a9b4d3af54d8
Hossein <hosseinheydari992020@gmail.com>
2026-05-09 07:07:43 +0330

    feat: Adding more settings for kde_plot

--- components/charts.py
+++ components/charts.py
@@ -31,15 +31,22 @@ class seaborn_chart:
             x: pd.Series = None,
             y: pd.Series = None,
             hue: pd.Series = None,
-            palette=None,
-            color: str = None,
             fill: bool = False,
             multiple: str = 'layer',
+            figsize: tuple[int, int] = (5, 5), dpi: int = 80,
+            main_title: str = "", xlabel: str = "",
+            ylabel: str = "", main_title_fontsize: int = 15,
+            xlabel_fontsize: int = 12, ylabel_fontsize: int = 12,
+            ax_fontsize: int = 11,
+            ax_mode: str = "both"
     ):
-        fig, ax = plt.subplots(figsize=(5, 5))
+        fig, ax = plt.subplots(figsize=figsize, dpi=dpi)
         sns.kdeplot(
             data=data, x=x, y=y, hue=hue,
-            palette=palette, color=color,
             fill=fill, multiple=multiple, ax=ax
         )
+        ax.set_title(label=main_title, fontsize=main_title_fontsize)
+        ax.set_xlabel(xlabel, fontsize=xlabel_fontsize)
+        ax.set_ylabel(ylabel, fontsize=ylabel_fontsize)
+        plt.tick_params(axis=ax_mode, labelsize=ax_fontsize)
         return fig
