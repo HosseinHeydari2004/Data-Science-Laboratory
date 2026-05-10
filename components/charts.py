943b3bfef87645d8ab6e1cfba68ca0f6afa3cf7b
Hossein <hosseinheydari992020@gmail.com>
2026-05-10 07:12:42 +0330

    feat: add 'boxplot' func and add setting

--- components/charts.py
+++ components/charts.py
@@ -260,3 +260,33 @@ class seaborn_chart:
         ax.set_ylabel(ylabel, fontsize=ylabel_fontsize)
         plt.tick_params(axis=ax_mode, labelsize=ax_fontsize)
         return fig
+
+    @classmethod
+    def boxplot(cls,
+                data: pd.DataFrame = None,
+                x: pd.Series = None,
+                y: pd.Series = None,
+                hue: pd.Series = None,
+                saturation: float = 0.75,
+                fill: bool = True,
+                gap: int | float = 0,
+                dpi: int = 80,
+                figsize: tuple[int, int] = (5, 5),
+                main_title: str = "", xlabel: str = "",
+                ylabel: str = "", main_title_fontsize: int = 15,
+                xlabel_fontsize: int = 12, ylabel_fontsize: int = 12,
+                ax_fontsize: int = 11,
+                ax_mode: str = "both"
+                ) -> plt.Figure:
+        fig, ax = plt.subplots(figsize=figsize, dpi=dpi)
+        sns.boxplot(
+            data=data, x=x, y=y,
+            hue=hue,
+            saturation=saturation, fill=fill,
+            gap=gap
+        )
+        ax.set_title(label=main_title, fontsize=main_title_fontsize)
+        ax.set_xlabel(xlabel, fontsize=xlabel_fontsize)
+        ax.set_ylabel(ylabel, fontsize=ylabel_fontsize)
+        plt.tick_params(axis=ax_mode, labelsize=ax_fontsize)
+        return fig
