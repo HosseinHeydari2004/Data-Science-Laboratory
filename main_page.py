3bb9c36bf1fcee5ac5869bb945c8ac37a9fcb5ba
Hossein <hosseinheydari992020@gmail.com>
2026-05-15 23:04:17 +0330

    rename: 'Main/home_page.py' -> 'app/home_page.py'

--- main_page.py
+++ main_page.py
@@ -2,8 +2,8 @@ import streamlit as st
 
 st.set_page_config(page_title="Data Science Lab", layout="wide")
 
-home_page = st.Page("Main/home_page.py", title="Welcome", icon="🏠", default=True)
-viz_page = st.Page("Main/page_2.py", title="Exploratory Data Analysis", icon="📊")
+home_page = st.Page("app/home_page.py", title="Welcome", icon="🏠", default=True)
+viz_page = st.Page("app/page_2.py", title="Exploratory Data Analysis", icon="📊")
 
 pg = st.navigation([home_page, viz_page])
 pg.run()
