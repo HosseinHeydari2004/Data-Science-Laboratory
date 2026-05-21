54f4e3ead16f822a4de3ce8b1032122a596c3892
Hossein <hosseinheydari992020@gmail.com>
2026-05-21 12:18:57 +0330

    feat(ui): add model page

--- main_page.py
+++ main_page.py
@@ -4,6 +4,7 @@ st.set_page_config(page_title="Data Science Lab", layout="wide")
 
 home_page = st.Page("app/pages/home_page.py", title="Welcome", icon="🏠", default=True)
 viz_page = st.Page("app/pages/page_2.py", title="Exploratory Data Analysis", icon="📊")
+model_page = st.Page("app/pages/page_3.py", title="Model Training", icon="🔧")
 
-pg = st.navigation([home_page, viz_page])
+pg = st.navigation([home_page, viz_page, model_page])
 pg.run()
