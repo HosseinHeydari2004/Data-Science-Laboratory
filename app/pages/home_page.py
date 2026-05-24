4a6ffb41e2822afa795cc51a1f044ef29577a2ae
Hossein <hosseinheydari992020@gmail.com>
2026-05-24 10:20:57 +0330

    Changes to the first page description

--- app/pages/home_page.py
+++ app/pages/home_page.py
@@ -7,7 +7,9 @@ st.write("# 🤖 Welcome to the Data Science and Machine Learning Laboratory!")
 st.markdown(
     """
      ### 📊 From Raw Data to Strategic Decisions in Seconds
-    This tool helps you analyze even the most complex data variables and predict the future – all without writing a single line of code.
+    This tool helps you analyze even the most complex data variables and predict the future. 
+     
+     **all without writing a single line of code.**
 
     ---
 
@@ -19,9 +21,10 @@ st.markdown(
     ---
 
     #### 🛠️ Getting Started
-    1. From the menu on the right, upload your data file (CSV or Excel).
-    2. Select your desired parameters in the settings section.
-    3. View analysis and prediction outputs in the tabs above.
+    1. From the menu on the below, upload your data file (CSV or Excel).
+    2. In the visualization page, you can modify your data, display it using a chart, and view your data.
+    3. Select your desired parameters in the model page.
+    4. View analysis and prediction outputs in the model page.
 
     ---
     ## Ready to get started? Upload your first file from the below!
@@ -35,7 +38,8 @@ uploaded_file = st.file_uploader(
     "Select a CSV or Excel file:",
     type=['csv', 'xlsx'],
     key="home_page_uploader",
-    help="upload your data up to limit 5MB"
+    max_upload_size=5,
+    help="upload your data up 5MB",
 )
 
 if uploaded_file is not None:
