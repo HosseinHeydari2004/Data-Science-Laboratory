d82e01af3fd09e09e14d8b94624602fa0679d83d
Hossein <hosseinheydari992020@gmail.com>
2026-05-25 10:07:18 +0330

    feat(plotly_charts): implement boxplot method with configurable aesthetics
    - Add oxplot class method using Plotly Express.
    - Support customization for titles, axes labels, fonts, and figure size.

--- components/charts.py
+++ components/charts.py
@@ -464,3 +464,31 @@ class plotly_charts:
             bargap=bargap
         )
         return fig
+
+    @classmethod
+    def boxplot(
+            cls, data: pd.DataFrame = None, x: pd.Series | str | object = None,
+            y: pd.Series | str | object = None, color: pd.Series | str | object = None,
+            hover_data: dict[Any] | list[Any] = None, log_x: bool | object = False,
+            log_y: bool | object = True, figsize: tuple[int, int] = (900, 600),
+            main_title: str = "", xlabel: str = "",
+            ylabel: str = "", main_title_fontsize: int = 15,
+            xlabel_fontsize: int = 12, ylabel_fontsize: int = 12,
+            tickfont_x: int = 14, tickfont_y: int = 14,
+            template: str | object = None
+    ) -> plt.Figure:
+        fig = px.box(
+            data_frame=data, x=x, y=y,
+            color=color, hover_data=hover_data, log_x=log_x,
+            log_y=log_y, width=figsize[0], height=figsize[1],
+        )
+        fig.update_layout(
+            title=main_title,
+            title_font=dict(size=main_title_fontsize),
+            xaxis_title=xlabel,
+            yaxis_title=ylabel,
+            xaxis=dict(tickfont=dict(size=tickfont_x), title_font=dict(size=xlabel_fontsize)),
+            yaxis=dict(tickfont=dict(size=tickfont_y), title_font=dict(size=ylabel_fontsize)),
+            template=template
+        )
+        return fig
