4d13f11372926f0f21584b8abf93f956aae96818
Hossein <hosseinheydari992020@gmail.com>
2026-06-03 12:10:15 +0330

    delete Auto_EDA func

--- Untils/Until.py
+++ Untils/Until.py
@@ -1,16 +1,6 @@
 import os
 
-import streamlit as st
 from pandas import DataFrame
-from ydata_profiling import ProfileReport
-
-
-@st.cache_data
-def Auto_EDA(data: DataFrame):
-    profile = ProfileReport(data, title="Data Profiling Report", explorative=True, minimal=True)
-    profile.to_file("report_Auto_EDA/report.html")
-    st.success(f"file save to 'report_Auto_EDA/report.html'", icon="✅")
-    return profile.to_html()
 
 
 def save_data(data: DataFrame, file_name: str = "processed_data.csv"):
